f08fc92c9a38ee80524d19804051c917468bb2f9
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-05-16 14:25:45 +0000

    added score tracking functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer () {
 
     if (isCorrect) {
         alert("Hey! You got it right :)")
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer}`)
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,10 +79,25 @@ function calculateCorrectAnswer () {
     }
 
 }
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore () {
 
-// function incrementScore ()
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
-// function incrementWrongAnswer ()
+}
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer () {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+
+}
 
 function displayAdditionQuestion (operand1, operand2){
     document.getElementById("operand1").textContent = operand1;
