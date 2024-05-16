6d3c0f688ddbbcf0b23db1deb2d66804fa3637a8
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-05-16 15:03:24 +0000

    did cleanup steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition")
 
 });
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame (gameType) {
+
+    document.getElementById("answer-box").value="";
+    document.getElementById("answer-box").focus();
 
     // creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -125,4 +134,4 @@ function displayMultiplyQuestion (operand1, operand2) {
     document.getElementById("operator").textContent = "x";
 }
 
-function displayDivideQuestion () 
+// function displayDivideQuestion () 
