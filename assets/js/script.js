5b535422cbb8ca6aa64f5d2a8b0b6d4d6cdbdef7
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-05-16 13:33:05 +0000

    added scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-console.log("connected")
+// wait for the DOM to finish loading before running the game
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicked ${gameType}`);
+            }
+        })
+    }
+})
 
 function runGame ()
 
